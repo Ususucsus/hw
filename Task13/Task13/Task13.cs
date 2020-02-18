ef16a37fc93e576adf8ef7590c2d3f290ab9fe8e
Ususucsus <usarti01@gmail.com>
2020-02-18 18:00:22 +0300

    git fix

---
+++ Task13/Task13/Task13.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task13
+{
+    class Task13
+    {
+        private static void BubbleSort(float[] arrFloats)
+        {
+            for (var i = 0; i < arrFloats.Length; ++i)
+            {
+                for (var j = 1; j < arrFloats.Length - i; ++j)
+                {
+                    if (arrFloats[j - 1] > arrFloats[j])
+                    {
+                        SwapFloats(ref arrFloats[j - 1], ref arrFloats[j]);
+                    }
+                }
+            }
+        }
+
+        private static void SwapFloats(ref float a, ref float b)
+        {
+            var t = a;
+            a = b;
+            b = t;
+        }
+
+        static void Main(string[] args)
+        {
+            var testArray = new float[10];
+            var random = new Random();
+
+            for (var i = 0; i < 10; ++i)
+            {
+                testArray[i] = random.Next(100);
+                Console.WriteLine(testArray[i]);
+            }
+
+            BubbleSort(testArray);
+
+            Console.WriteLine("Sorted:");
+            foreach (var f in testArray)
+            {
+                Console.WriteLine(f);
+            }
+        }
+    }
+}
