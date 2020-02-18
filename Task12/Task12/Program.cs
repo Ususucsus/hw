e40c8332aeb78752cb0ba708db6334a35fd6db06
Ususucsus <usarti01@gmail.com>
2020-02-18 18:00:00 +0300

    git fix

---
+++ Task12/Task12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task12
+{
+    class Program
+    {
+        private static int Fibonacci(int n)
+        {
+            var fib1 = 1;
+            var fib2 = 1;
+
+            for (var i = 2; i < n; ++i)
+            {
+                var tempFib = fib2;
+                fib2 += fib1;
+                fib1 = tempFib;
+            }
+
+            return fib2;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Fibonacci(3): {Fibonacci(3)}");
+        }
+    }
+}
