c3f42669a0b3db8e63c283e322d600b3bcc3a182
Ususucsus <usarti01@gmail.com>
2020-02-24 15:37:50 +0300

    first

---
+++ Task23/Task23/StackArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task23
+{
+    internal class StackArray : IStack
+    {
+        public StackArray()
+        {
+            array = new int[1000];
+        }
+
+        public void Push(int value)
+        {
+            if (head >= 1000)
+                throw new InvalidOperationException();
+
+            array[head++] = value;
+        }
+
+        public int Pop()
+        {
+            if (this.IsEmpty())
+                throw new InvalidOperationException();
+
+            var popValue = array[head - 1];
+            --head;
+            return popValue;
+        }
+
+        public bool IsEmpty()
+            => head == 0;
+
+        private readonly int[] array;
+        private int head;
+
+        public const int MaxSize = 1000;
+    }
+}
---
+++ Task23/Task23/StackCalculator.cs
@@ -0,0 +1,54 @@
+﻿namespace Task23
+{
+    internal class StackCalculator
+    {
+        public StackCalculator(IStack stack)
+        {
+            this.stack = stack;
+        }
+
+        public void Push(int value)
+        {
+            stack.Push(value);
+        }
+
+        public int Pop()
+        {
+            return stack.Pop();
+        }
+
+        public void Add()
+        {
+            var value2 = stack.Pop();
+            var value1 = stack.Pop();
+
+            stack.Push(value1 + value2);
+        }
+
+        public void Subtract()
+        {
+            var value2 = stack.Pop();
+            var value1 = stack.Pop();
+
+            stack.Push(value1 - value2);
+        }
+
+        public void Multiply()
+        {
+            var value2 = stack.Pop();
+            var value1 = stack.Pop();
+
+            stack.Push(value1 * value2);
+        }
+
+        public void Divide()
+        {
+            var value2 = stack.Pop();
+            var value1 = stack.Pop();
+
+            stack.Push(value1 / value2);
+        }
+
+        private readonly IStack stack;
+    }
+}
