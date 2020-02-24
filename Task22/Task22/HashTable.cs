9beef4d1aea6cebdbff1c94f544d325325e449ee
Ususucsus <usarti01@gmail.com>
2020-02-24 14:04:59 +0300

    first

---
+++ Task22/Task22/HashTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task22
+{
+    internal class HashTable
+    {
+        public HashTable(int size)
+        {
+            this.size = size;
+            array = new List[size];
+
+            for (var i = 0; i < size; ++i) 
+                array[i] = new List(new string[] { } );
+        }
+
+        public void Insert(string value)
+        {
+            if (this.ContainsValue(value))
+                throw new ArgumentException();
+
+            var hashValue = GetHash(value);
+
+            array[hashValue].Insert(array[hashValue].Length(), value);
+        }
+
+        public bool ContainsValue(string value)
+        {
+            var hashValue = GetHash(value);
+
+            for (var i = 0; i < array[hashValue].Length(); ++i)
+            {
+                if (array[hashValue].GetValue(i) == value)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void Remove(string value)
+        {
+            var hashValue = GetHash(value);
+
+            for (var i = 0; i < array[hashValue].Length(); ++i)
+            {
+                if (array[hashValue].GetValue(i) == value)
+                {
+                    array[hashValue].Remove(i);
+                    return;
+                }
+            }
+        }
+
+        private int GetHash(string value)
+            => Math.Abs(value.GetHashCode()) % size;
+
+        private readonly int size;
+        private readonly List[] array;
+    }
+}
---
+++ Task22/Task22/List.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace Task22
+{
+    internal class List
+    {
+        private class Node
+        {
+            public Node(string data, Node next)
+            {
+                this.Data = data;
+                this.Next = next;
+            }
+
+            public string Data;
+            public Node Next;
+        }
+
+        public List(string[] array)
+        {
+            if (array.Length == 0)
+            {
+                head = null;
+                return;
+            }
+
+            head = new Node(array[0], null);
+            var prev = head;
+
+            for (var i = 1; i < array.Length; ++i)
+            {
+                prev.Next = new Node(array[i], null);
+                prev = prev.Next;
+            }
+
+            this.length = array.Length;
+        }
+
+        public string GetValue(int position)
+        {
+            if (position < 0 || position >= length)
+                throw new ArgumentOutOfRangeException();
+
+            return GetNode(position).Data;
+        }
+
+        public void SetValue(int position, string value)
+        {
+            if (position < 0 || position >= length)
+                throw new ArgumentOutOfRangeException();
+
+            GetNode(position).Data = value;
+        }
+
+        public void Insert(int position, string value)
+        {
+            if (position < 0 || position > length)
+                throw new ArgumentOutOfRangeException();
+
+            var newNode = new Node(value, null);
+
+            if (position == 0)
+            {
+                var tempNode = head;
+                head = newNode;
+                newNode.Next = tempNode;
+            }
+            else
+            {
+                var prevNode = GetNode(position - 1);
+                newNode.Next = prevNode.Next;
+                prevNode.Next = newNode;
+            }
+
+            ++length;
+        }
+
+        public void Remove(int position)
+        {
+            if (position < 0 || position >= length)
+                throw new ArgumentOutOfRangeException();
+
+            if (position == 0)
+                head = head.Next;
+            else
+            {
+                var prevNode = GetNode(position - 1);
+                prevNode.Next = GetNode(position).Next;
+            }
+
+            --length;
+        }
+
+        public int Length()
+            => this.length;
+
+        public bool IsEmpty()
+            => this.length == 0;
+
+        private Node GetNode(int position)
+        {
+            if (position < 0 || position >= length)
+                return null;
+
+            var currentNode = head;
+            for (var i = 0; i < position; ++i)
+                currentNode = currentNode.Next;
+
+            return currentNode;
+        }
+
+        public override string ToString()
+        {
+            var listString = "List: ";
+
+            if (head == null)
+                return listString;
+
+            listString += $"{head.Data}";
+
+            var currentNode = head.Next;
+            while (currentNode != null)
+            {
+                listString += $", {currentNode.Data}";
+
+                currentNode = currentNode.Next;
+            }
+
+            return listString;
+        }
+
+        private Node head;
+        private int length;
+    }
+}
