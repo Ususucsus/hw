b0abed70f27c8704cce7ed8f0772fa1f09394660
Ususucsus <usarti01@gmail.com>
2020-02-29 20:52:59 +0300

    remove unused usings

--- Task23/Task23.Test/StackCalculatorViaListTest.cs
+++ Task23/Task23.Test/StackCalculatorViaListTest.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Text;
 using NUnit.Framework;
 
 namespace Task23.Test
--- Task23/Task23.Test/StackListTests.cs
+++ Task23/Task23.Test/StackListTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Security;
 using NUnit.Framework;
 
 namespace Task23.Test
