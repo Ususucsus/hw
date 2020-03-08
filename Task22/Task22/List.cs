8b672302179b265b52f068b6b035d3344e2e79f0
Ususucsus <usarti01@gmail.com>
2020-03-08 19:02:20 +0300

    add List.cs documetation nullable enable

--- Task22/Task22/List.cs
+++ Task22/Task22/List.cs
@@ -2,20 +2,42 @@
 
 namespace Task22
 {
+    /// <summary>
+    /// Represents a list of strings. Provides methods to insert, remove and change strings in the list.
+    /// </summary>
     internal class List
     {
-        private class Node
+        /// <summary>
+        /// Represents a list element with data and pointer to next list element.
+        /// </summary>
+        private class ListElement
         {
-            public Node(string data, Node next)
+            /// <summary>
+            /// Initializes a new instance of the <see cref="ListElement"/> class.
+            /// </summary>
+            /// <param name="data">string value</param>
+            /// <param name="next">pointer to next list element</param>
+            public ListElement(string data, ListElement? next)
             {
                 this.Data = data;
                 this.Next = next;
             }
 
+            /// <summary>
+            /// List element value.
+            /// </summary>
             public string Data;
-            public Node Next;
+
+            /// <summary>
+            /// Pointer to next list element.
+            /// </summary>
+            public ListElement? Next;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="List"/> class.
+        /// </summary>
+        /// <param name="array">array from which the list will be built</param>
         public List(string[] array)
         {
             if (array.Length == 0)
@@ -24,40 +46,58 @@ namespace Task22
                 return;
             }
 
-            head = new Node(array[0], null);
+            head = new ListElement(array[0], null);
             var prev = head;
 
             for (var i = 1; i < array.Length; ++i)
             {
-                prev.Next = new Node(array[i], null);
+                prev.Next = new ListElement(array[i], null);
                 prev = prev.Next;
             }
 
             this.length = array.Length;
         }
 
+        /// <summary>
+        /// Returns value of element at given position.
+        /// </summary>
+        /// <param name="position">Position of element which should be returned</param>
+        /// <exception cref="ArgumentOutOfRangeException">Position is less than 0 or more or equal than length of the list</exception>
+        /// <returns>Value of element at given position</returns>
         public string GetValue(int position)
         {
             if (position < 0 || position >= length)
                 throw new ArgumentOutOfRangeException();
 
-            return GetNode(position).Data;
+            return GetNode(position)!.Data;
         }
 
+        /// <summary>
+        /// Set value of element at given position.
+        /// </summary>
+        /// <param name="position">Position of element which value should be changed</param>
+        /// <param name="value">Value which should be set</param>
+        /// <exception cref="ArgumentOutOfRangeException">Position is less than 0 or more or equal than length of the list</exception>
         public void SetValue(int position, string value)
         {
             if (position < 0 || position >= length)
                 throw new ArgumentOutOfRangeException();
 
-            GetNode(position).Data = value;
+            GetNode(position)!.Data = value;
         }
 
+        /// <summary>
+        /// Inserts new list element to given position.
+        /// </summary>
+        /// <param name="position">Position of the new list element</param>
+        /// <param name="value">Value of the new list element</param>
+        /// <exception cref="ArgumentOutOfRangeException">Position is less than 0 or more than length of the list</exception>
         public void Insert(int position, string value)
         {
             if (position < 0 || position > length)
                 throw new ArgumentOutOfRangeException();
 
-            var newNode = new Node(value, null);
+            var newNode = new ListElement(value, null);
 
             if (position == 0)
             {
@@ -68,47 +108,69 @@ namespace Task22
             else
             {
                 var prevNode = GetNode(position - 1);
-                newNode.Next = prevNode.Next;
+                newNode.Next = prevNode!.Next;
                 prevNode.Next = newNode;
             }
 
             ++length;
         }
 
+        /// <summary>
+        /// Removes list element at given position.
+        /// </summary>
+        /// <param name="position">Position of element which should be removed</param>
+        /// <exception cref="ArgumentOutOfRangeException">Position is less than 0 or more or equal than length of the list</exception>
         public void Remove(int position)
         {
             if (position < 0 || position >= length)
                 throw new ArgumentOutOfRangeException();
 
             if (position == 0)
-                head = head.Next;
+                head = head!.Next;
             else
             {
                 var prevNode = GetNode(position - 1);
-                prevNode.Next = GetNode(position).Next;
+                prevNode!.Next = GetNode(position)!.Next;
             }
 
             --length;
         }
 
+        /// <summary>
+        /// Returns length of the list.
+        /// </summary>
+        /// <returns>Length of the list</returns>
         public int Length()
             => this.length;
 
+        /// <summary>
+        /// Checks if the list is empty.
+        /// </summary>
+        /// <returns>true if the list is empty, false otherwise</returns>
         public bool IsEmpty()
             => this.length == 0;
 
-        private Node GetNode(int position)
+        /// <summary>
+        /// Returns list element at given position.
+        /// </summary>
+        /// <param name="position">Position of element which should be returned.</param>
+        /// <returns>List element at given position or null when list element isn't exist at given position</returns>
+        private ListElement? GetNode(int position)
         {
             if (position < 0 || position >= length)
                 return null;
 
             var currentNode = head;
             for (var i = 0; i < position; ++i)
-                currentNode = currentNode.Next;
+                currentNode = currentNode!.Next;
 
             return currentNode;
         }
 
+        /// <summary>
+        /// Returns string representation of the list.
+        /// </summary>
+        /// <returns>string representation of the list</returns>
         public override string ToString()
         {
             var listString = "List: ";
@@ -129,7 +191,14 @@ namespace Task22
             return listString;
         }
 
-        private Node head;
+        /// <summary>
+        /// Head list element.
+        /// </summary>
+        private ListElement? head;
+
+        /// <summary>
+        /// Length of the list.
+        /// </summary>
         private int length;
     }
 }
