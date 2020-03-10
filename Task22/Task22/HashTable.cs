08c52f11c5a95e3659264f36dfd24d116b07ee55
Ususucsus <usarti01@gmail.com>
2020-03-10 21:56:17 +0300

    added HashTable.cs documentation

--- Task22/Task22/HashTable.cs
+++ Task22/Task22/HashTable.cs
@@ -2,8 +2,15 @@
 
 namespace Task22
 {
+    /// <summary>
+    /// Represents a set of values. Provides methods to insert, remove and check for containing.
+    /// </summary>
     public class HashTable
     {
+        /// <summary>
+        /// Initializes a new instance of <see cref="HashTable"/> class.
+        /// </summary>
+        /// <param name="size">Maximum number of different hash codes</param>
         public HashTable(int size)
         {
             this.size = size;
@@ -13,6 +20,11 @@ namespace Task22
                 array[i] = new List(new string[] { } );
         }
 
+        /// <summary>
+        /// Inserts value to the hash table.
+        /// </summary>
+        /// <param name="value">Value to be inserted</param>
+        /// <exception cref="ArgumentException">Value already contains in hash table</exception>
         public void Insert(string value)
         {
             if (this.ContainsValue(value))
@@ -23,6 +35,11 @@ namespace Task22
             array[hashValue].Insert(array[hashValue].Length(), value);
         }
 
+        /// <summary>
+        /// Checks if value contains in hash table
+        /// </summary>
+        /// <param name="value">Value to be checked</param>
+        /// <returns>true if value contains in hash table, false otherwise</returns>
         public bool ContainsValue(string value)
         {
             var hashValue = GetHash(value);
@@ -36,6 +53,10 @@ namespace Task22
             return false;
         }
 
+        /// <summary>
+        /// Removes value from hash table
+        /// </summary>
+        /// <param name="value">Value to be removed</param>
         public void Remove(string value)
         {
             var hashValue = GetHash(value);
@@ -50,10 +71,22 @@ namespace Task22
             }
         }
 
+        /// <summary>
+        /// Returns hash code of value within limits.
+        /// </summary>
+        /// <param name="value">Value form which hash will be calculated</param>
+        /// <returns>Hash within limits</returns>
         private int GetHash(string value)
             => Math.Abs(value.GetHashCode()) % size;
 
+        /// <summary>
+        /// Maximum number of different hash codes.
+        /// </summary>
         private readonly int size;
+
+        /// <summary>
+        /// Array to contain lists with same values with same hash codes.
+        /// </summary>
         private readonly List[] array;
     }
 }
