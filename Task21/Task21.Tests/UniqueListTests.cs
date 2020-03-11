6d958707a7b6919b0db2db9eab6ac69847297b3e
Ususucsus <usarti01@gmail.com>
2020-03-11 17:09:18 +0300

    added UniqueList class

---
+++ Task21/Task21.Tests/UniqueListTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task21.Test
+{
+    public class UniqueListTests
+    {
+        private UniqueList uniqueList;
+
+        [SetUp]
+        public void Setup()
+        {
+            uniqueList = new UniqueList();
+        }
+
+        [Test]
+        public void InsertDifferentElementsShouldWorkCorrect()
+        {
+            uniqueList.Insert(0, 1);
+            uniqueList.Insert(1, 2);
+
+            Assert.IsTrue(uniqueList.ContainsValue(1));
+            Assert.IsTrue(uniqueList.ContainsValue(2));
+        }
+
+        [Test]
+        public void InsertSameElementsShouldThrowException()
+        {
+            uniqueList.Insert(0, 1);
+
+            Assert.Throws<InvalidOperationException>(() => uniqueList.Insert(0, 1));
+        }
+    }
+}
---
+++ Task21/Task21/UniqueList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task21
+{
+    public class UniqueList : List
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="UniqueList"/> class.
+        /// </summary>
+        public UniqueList()
+            : base()
+        {
+
+        }
+
+        /// <summary>
+        /// Inserts value to list to given position.
+        /// </summary>
+        /// <param name="position">Position of new list element</param>
+        /// <param name="value">Value to be inserted</param>
+        /// <exception cref="InvalidOperationException">If element already contains in list</exception>
+        public override void Insert(int position, int value)
+        {
+            if (base.ContainsValue(value) == true)
+            {
+                throw new InvalidOperationException();
+            }
+
+            base.Insert(position, value);
+        }
+    }
+}
