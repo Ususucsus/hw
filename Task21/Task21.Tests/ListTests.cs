d4fe236b5ef6d1140e918cc67d652d8eb11a0051
Ususucsus <usarti01@gmail.com>
2020-03-11 16:40:35 +0300

    added List.cs

---
+++ Task21/Task21.Tests/ListTests.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Task21.Test
+{
+    public class ListTests
+    {
+        private List list;
+        private List arrayList;
+
+        [SetUp]
+        public void Setup()
+        {
+            list = new List();
+            arrayList = new List(new int[] { 1, 2 });
+        }
+
+        [Test]
+        public void NewListShouldBeEmpty()
+        {
+            Assert.IsTrue(list.IsEmpty());
+        }
+
+        [Test]
+        public void InitializedListWithArrayShouldNotBeEmpty()
+        {
+            Assert.IsFalse(arrayList.IsEmpty());
+        }
+
+        [Test]
+        public void LengthOfInitializedListShouldBeCorrect()
+        {
+            Assert.AreEqual(arrayList.Length(), 2);
+        }
+
+        [Test]
+        public void LengthOfNewListShouldBeZero()
+        {
+            Assert.AreEqual(list.Length(), 0);
+        }
+
+        [Test]
+        public void GetValueWithinLimitsShouldReturnCorrectValue()
+        {
+            Assert.AreEqual(arrayList.GetValue(1), 2);
+        }
+
+        [Test]
+        public void GetValueBeyondLimitsShouldThrowException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => arrayList.GetValue(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => arrayList.GetValue(2));
+            Assert.Throws<ArgumentOutOfRangeException>(() => list.GetValue(0));
+        }
+
+        [Test]
+        public void SetValueShouldChangeElementValue()
+        {
+            arrayList.SetValue(0, 3);
+
+            Assert.AreEqual(arrayList.GetValue(0), 3);
+        }
+
+        [Test]
+        public void SetValueBeyondLimitsShouldThrowException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => arrayList.SetValue(-1, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => arrayList.SetValue(2, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => list.SetValue(-1, 0));
+        }
+
+        [Test]
+        public void RemoveFirstElementShouldWorkCorrect()
+        {
+            var lengthBeforeRemove = arrayList.Length();
+            arrayList.Remove(0);
+
+            Assert.AreEqual(arrayList.Length(), lengthBeforeRemove - 1);
+            Assert.AreEqual(arrayList.GetValue(0), 2);
+        }
+
+        [Test]
+        public void RemoveAnyElementShouldWorkCorrect()
+        {
+            var lengthBeforeRemove = arrayList.Length();
+            arrayList.Remove(1);
+
+            Assert.AreEqual(arrayList.Length(), lengthBeforeRemove - 1);
+            Assert.AreEqual(arrayList.GetValue(0), 1);
+        }
+
+
+        [Test]
+        public void RemoveBeyondLimitsShouldThrowException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => arrayList.Remove(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => arrayList.Remove(2));
+            Assert.Throws<ArgumentOutOfRangeException>(() => list.Remove(0));
+        }
+
+        [Test]
+        public void InsertShouldInsertElementToList()
+        {
+            var lengthBeforeInsert = arrayList.Length();
+            arrayList.Insert(1, 3);
+
+            Assert.AreEqual(arrayList.Length(), lengthBeforeInsert + 1);
+            Assert.AreEqual(arrayList.GetValue(2), 2);
+            Assert.AreEqual(arrayList.GetValue(1), 3);
+        }
+
+        [Test]
+        public void InsertAtFirstPositionShouldWorkCorrect()
+        {
+            var lengthBeforeInsert = arrayList.Length();
+            arrayList.Insert(0, 3);
+
+            Assert.AreEqual(arrayList.Length(), lengthBeforeInsert + 1);
+            Assert.AreEqual(arrayList.GetValue(1), 1);
+            Assert.AreEqual(arrayList.GetValue(0), 3);
+        }
+
+        [Test]
+        public void InsertBeyondLimitsShouldThrowException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => arrayList.Insert(-1, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => arrayList.Insert(3, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => list.Insert(1, 0));
+        }
+
+        [Test]
+        public void ListShouldBeEmptyAfterRemoveAllElements()
+        {
+            arrayList.Remove(0);
+            arrayList.Remove(0);
+
+            Assert.IsTrue(arrayList.IsEmpty());
+        }
+
+        [Test]
+        public void ListInitializedWithEmptyArrayShouldBeEmpty()
+        {
+            var newList = new List(new int[] { });
+
+            Assert.IsTrue(newList.IsEmpty());
+        }
+    }
+}
--- Task21/Task21/List.cs
+++ Task21/Task21/List.cs
@@ -2,20 +2,42 @@
 
 namespace Task21
 {
-    internal class List
+    /// <summary>
+    /// Represents a list of integers. Provides methods to insert, remove and change integers in the list.
+    /// </summary>
+    public class List
     {
-        private class Node
+        /// <summary>
+        /// Provides a list element with data and pointer to next list element.
+        /// </summary>
+        private class ListElement
         {
-            public Node(int data, Node next)
+            /// <summary>
+            /// Initializes a new instance of <see cref="ListElement"/> class.
+            /// </summary>
+            /// <param name="data">integer value</param>
+            /// <param name="next">Pointer to next list element</param>
+            public ListElement(int data, ListElement next)
             {
                 this.Data = data;
                 this.Next = next;
             }
 
+            /// <summary>
+            /// List element value.
+            /// </summary>
             public int Data;
-            public Node Next;
+
+            /// <summary>
+            /// Pointer to next list element.
+            /// </summary>
+            public ListElement Next;
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="List"/> class.
+        /// </summary>
+        /// <param name="array">Array from which list will be build</param>
         public List(int[] array)
         {
             if (array.Length == 0)
@@ -24,18 +46,32 @@ namespace Task21
                 return;
             }
 
-            head = new Node(array[0], null);
+            head = new ListElement(array[0], null);
             var prev = head;
 
             for (var i = 1; i < array.Length; ++i)
             {
-                prev.Next = new Node(array[i], null);
+                prev.Next = new ListElement(array[i], null);
                 prev = prev.Next;
             }
 
             this.length = array.Length;
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="List"/> class.
+        /// </summary>
+        public List()
+        {
+            head = null;
+        }
+
+        /// <summary>
+        /// Returns value of element at given position.
+        /// </summary>
+        /// <param name="position">Position of element which value should be returned</param>
+        /// <exception cref="ArgumentOutOfRangeException">Position is less than 0 or more or equal than length of the list</exception>
+        /// <returns>Value of element</returns>
         public int GetValue(int position)
         {
             if (position < 0 || position >= length)
@@ -44,6 +80,12 @@ namespace Task21
             return GetNode(position).Data;
         }
 
+        /// <summary>
+        /// Set value of element at given position.
+        /// </summary>
+        /// <param name="position">Position of element which value should be changed</param>
+        /// <param name="value">Value which should be set</param>
+        /// <exception cref="ArgumentOutOfRangeException">Position is less than 0 or more or equal than length of the list</exception>
         public void SetValue(int position, int value)
         {
             if (position < 0 || position >= length)
@@ -52,12 +94,18 @@ namespace Task21
             GetNode(position).Data = value;
         }
 
+        /// <summary>
+        /// Inserts new list element to given position.
+        /// </summary>
+        /// <param name="position">Position of the new list element</param>
+        /// <param name="value">Value of the new list element</param>
+        /// <exception cref="ArgumentOutOfRangeException">Position is less than 0 or more than length of the list</exception>
         public void Insert(int position, int value)
         {
             if (position < 0 || position > length)
                 throw new ArgumentOutOfRangeException();
 
-            var newNode = new Node(value, null);
+            var newNode = new ListElement(value, null);
 
             if (position == 0)
             {
@@ -75,6 +123,11 @@ namespace Task21
             ++length;
         }
 
+        /// <summary>
+        /// Removes list element at given position.
+        /// </summary>
+        /// <param name="position">Position of element which should be removed</param>
+        /// <exception cref="ArgumentOutOfRangeException">Position is less than 0 or more or equal than length of the list</exception>
         public void Remove(int position)
         {
             if (position < 0 || position >= length)
@@ -91,13 +144,26 @@ namespace Task21
             --length;
         }
 
+        /// <summary>
+        /// Returns length of the list.
+        /// </summary>
+        /// <returns>Length of the list</returns>
         public int Length()
             => this.length;
 
+        /// <summary>
+        /// Checks if the list is empty.
+        /// </summary>
+        /// <returns>true if the list is empty, false otherwise</returns>
         public bool IsEmpty()
             => this.length == 0;
 
-        private Node GetNode(int position)
+        /// <summary>
+        /// Returns list element at given position.
+        /// </summary>
+        /// <param name="position">Position of element which should be returned.</param>
+        /// <returns>List element at given position or null when list element isn't exist at given position</returns>
+        private ListElement GetNode(int position)
         {
             if (position < 0 || position >= length)
                 return null;
@@ -109,6 +175,10 @@ namespace Task21
             return currentNode;
         }
 
+        /// <summary>
+        /// Returns string representation of the list.
+        /// </summary>
+        /// <returns>string representation of the list</returns>
         public override string ToString()
         {
             var listString = "List: ";
@@ -129,7 +199,14 @@ namespace Task21
             return listString;
         }
 
-        private Node head;
+        /// <summary>
+        /// Head list element.
+        /// </summary>
+        private ListElement head;
+
+        /// <summary>
+        /// Length of the list.
+        /// </summary>
         private int length;
     }
 }
